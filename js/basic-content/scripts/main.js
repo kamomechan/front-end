a134a3b6bbc333317d39f437eb34d8d244c3e0d0
kamomechan <texiachann@gmail.com>
2025-05-14 11:49:57 +0800

    update main.js

--- js/basic-content/scripts/main.js
+++ js/basic-content/scripts/main.js
@@ -1,2 +1,19 @@
+// DOM文档对象模型，使用document对象来对文档内容进行操作
+// querySelector()函数获取元素内容并将其储存在myHeading变量中
 const myHeading = document.querySelector("h1");
+// textContent对象用来改变元素内容
 myHeading.textContent = "Hello world!";
+
+// 变量是值储存的容器
+// 声明变量用 let + 变量名
+let myVariable;
+// 变量可以进行赋值或更改
+myVariable=1;
+myVariable=2;
+
+// 变量可以储存多个数据类型
+// 1.字符串 用双引号或单引号括起来的值。 let myVariable="hello";或者let myVariable='hello';
+// 2.数字 没有被引号括起来的数字。 let myVariable=10;
+// 3.布尔 真假值 ture/false。 let myVariable=true;或let myVariable=false;
+// 4.数组 存储多个值的结构。 let myVariable=[1,'hello',2] 可以用myVariable[0]来获取数组成员等等
+// 5.对象 任意内容。 const myHeading = document.querySelector("h1");以及上面所有的示例
